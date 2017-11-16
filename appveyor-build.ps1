6b1282f1185ddb55d53d51ba3c776e7997309bdb
Stefan Helfrich <stefan.helfrich@uni-konstanz.de>
2017-11-16 07:43:14 +0100

    Add appveyor-build.ps1
    Let's you build (i.e. `mvn install`) on AppVeyor.

---
+++ appveyor-build.ps1
@@ -0,0 +1,16 @@
+"== Path (Execution Environment) =="
+$Env:Path
+
+"`n== Path (Machine) =="
+[Environment]::GetEnvironmentVariable("Path", "Machine")
+
+"`n== Maven =="
+& "mvn" "-B" "--version"
+
+"`n== Java =="
+& "cmd.exe" /c 'java -version 2>&1' # Output goes to STDERR which makes this invocation fail the build in PS
+
+"`n== Building the artifact locally =="
+& "mvn" "-B" "install" 2> $null
+
+exit $LASTEXITCODE
